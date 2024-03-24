1c3d92ab5366c762c5eb369251bcf9baecb7dec0
Upendra Pal <up06072000@gmail.com>
2024-03-24 14:02:51 +0530

    Update package.json

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const cors=require("cors")
 const express=require("express");
 require("express-async-errors")
+
 const { connection } = require("./config/db");
 const { userRoute } = require("./routes/user.route");
 const { postRoute } = require("./routes/post.route");
-
+const PORT = process.env.PORT ||8080
 const app= express();
 app.use(express.json())
 app.use(cors())
@@ -21,7 +22,7 @@ app.use((err,req, res, next) => {
     res.status(err.status||500).send({err, msg: "erro acuurse" });
 });
 
- app.listen(8080,async()=>{
+ app.listen(PORT,async()=>{
    try {
     await connection
     console.log("server running on port 8080");
